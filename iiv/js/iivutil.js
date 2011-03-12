5bc6b641f95c703a7828db5d36942a1f6ed3e2c8
Bishop <bishop@Bishops-Mac-mini.local>
2011-03-12 17:04:55 -0400

    Moved javascript to iivutil.js

--- iiv/js/iivutil.js
+++ iiv/js/iivutil.js
@@ -125,3 +125,177 @@ function getHighlightCoordinatesCallback(retData){
         drawBox(obj);
     }
 }
+/**
+ * setupPopControl
+ * 
+ */
+//added by sfb
+function setupPopControl(){
+	   map.addControl(polyControl);
+	   polyControl.activate();
+	   document.getElementById('iiv-image-panel').style.cursor = 'crosshair';
+}
+//Sabina
+function setupPopControlforPolygon(){	   
+	   map.addControl(mulpolyControl);
+	   mulpolyControl.activate();	 
+	   document.getElementById('iiv-image-panel').style.cursor = 'crosshair';
+}
+//added by sfb
+/**
+ * onPopupClose
+ * Close the popup
+ */
+function onPopupClose(evt) {
+    // 'this' is the popup.
+	//polyControl.unselect(this.feature);
+	//alert(evt);
+    document.getElementById('iiv-image-panel').style.cursor = 'move';
+    map.removePopup(popup);
+    popup.destroy();
+    popup = null;
+    polyControl.box.clear();
+   
+    map.removeControl(polyControl);
+    //TODO:set focus back to map so selection doesn't occur again
+}
+
+/**
+ * boxNotice
+ * When the selects a polygon
+ */
+//added by sfb
+function boxNotice(geom) {
+	 document.getElementById('iiv-image-panel').style.cursor = 'crosshair';
+    var feature = new OpenLayers.Feature.Vector(geom, null, {
+        strokeColor: "#0033ff",
+        strokeOpacity: 0.7,
+        strokeWidth: 5
+    });
+    featureSelect(feature);
+    //sortOutButtons(oDragPanCtrl, false);
+}//boxNotice
+
+//Sabina
+function drawPolygon(geom)
+{
+    //Define Layer 
+    var vectorLayer = new OpenLayers.Layer.Vector("Simple Geometry", {});		
+    var polygonFeature = new OpenLayers.Feature.Vector(geom, null, {
+        strokeColor: "#ff0000", //color of the line on features
+        fillColor:"green", //color used to fill polygon, default is #ee9900
+        strokeOpacity: 0.2,
+        fillOpacity:0.4, //This is the opacity used for filling in Polygons	
+        strokeWidth: 1 //default is 1
+    });  
+   map.addLayer(vectorLayer);// add it to map
+   vectorLayer.addFeatures(polygonFeature);//redraw with the polygon points we have
+}
+
+
+
+/**
+ *  featureSelect - show dialog for selection
+*/
+// added by sfb
+function featureSelect(feature) {
+    //need to have a minimum selection
+    if (feature.geometry.getArea() < 10)
+        return;
+    document.getElementById('iiv-image-panel').style.cursor = 'move';
+    /*
+    */
+    selectedFeature = feature;
+    var bounds = selectedFeature.geometry.getBounds();
+    
+    //leaving in for now, we may need this sfb
+    //var currZoom = map.getZoom();
+   // var canvasSize = mapcanvas;
+    //if (currZoom == ZOOMIN)
+    //    canvasSize = canvasMax;
+    //if (currZoom == ZOOMOUT)
+     //   canvasSize = canvasMin;
+
+    /*
+    alert (sortOutSelectedBox(bounds.toArray(), canvasSize, parseInt(bounds.getWidth()),
+        parseInt(bounds.getHeight())));
+    */
+    
+    var viewerUI = this;
+    var title = "www.islandnewspapers.ca";
+    popup = new OpenLayers.Popup.FramedCloud("Region", 
+        feature.geometry.getBounds().getCenterLonLat(),
+        null,
+        "<div id='popupdiv'><form><strong>Selection from <i>" +
+        title +"</i></strong><br/>\n" +
+        "Please enter an annotation for the selected information, or<br/>\n" +
+        "close this popup if you want to make another selection.<br/><br/>" +
+        "<strong>Annotation Text:</strong><br/>" + 
+        "<textarea name='annotationText' id='annotationText' cols='40' rows='6' wrap></textarea><br/>" + 
+        "<div id='saveData' align='right'"+
+        	"<input type='checkbox' name='annotationPublic' id='annotationPublic' value='1' checked /> Public "+
+        	"<a href='#' onclick=saveAnnotation($('#annotationText').val(),$('input[name=annotationPublic]:checked').val());>Save</a>"+
+        "</div>" + 
+        "<br clear=\"left\"/>" +
+       // sortOutSelectedBox(bounds.toArray(), canvasSize, parseInt(bounds.getWidth()),
+       // parseInt(bounds.getHeight())) +
+        "<br clear=\"left\"/>" +
+        //"<i>Coming Soon: annotate this selection!</i>" +
+        "</form></div>", 
+        null, true, onPopupClose);
+       feature.popup = popup;
+    map.addPopup(popup);
+}//featureSelect
+/**
+ * saveAnnotation
+ * 
+ */
+ 
+function saveAnnotation(annotationText,publicOn){
+	alert("i would have saved:" + annotationText + "\n" +"Public=" + publicOn);
+	//TODO: finish save logic
+	//TODO:close popup
+	
+}
+
+function drawBox(obj){
+    var styleMap = new OpenLayers.StyleMap(OpenLayers.Util.applyDefaults(
+        {fillColor: "yellow", fillOpacity: 0.3, strokeColor: "green"},
+        OpenLayers.Feature.Vector.style["default"]));
+
+                
+	// y coordinates in the words XY start at 0 from the top where as
+	// open layers consideres 0 the bottom.  Take the complement of the
+	// given y value relative to the map height to invert the value. Note that in the Bounds function
+	// the top value of the words xy data is passed to the bottom parameter in the function
+	// because of this inversion.
+	
+	var mapHeight = this.map.maxExtent.top;
+	bounds = new OpenLayers.Bounds(obj.l, (mapHeight - obj.t), obj.r, (mapHeight - obj.b));
+	                    
+	hightlightBox = new OpenLayers.Feature.Vector(bounds.toGeometry(),{styleMap: styleMap});
+	
+	var highlightLayer = getHighlightLayer();                     
+	highlightLayer.addFeatures(hightlightBox);                   
+                            
+}//drawBox
+
+function getHighlightLayer(){
+    var highlightLayer = this.map.getLayersByName("highlightLayer");
+    if(highlightLayer.length > 0){
+        return highlightLayer[0];
+    }
+    else{
+        var styleMap = new OpenLayers.StyleMap(OpenLayers.Util.applyDefaults(
+        {fillColor: "yellow", fillOpacity: 0.3, strokeColor: "green"},
+        OpenLayers.Feature.Vector.style["default"]));
+        
+        highlightLayer = new OpenLayers.Layer.Vector("highlightLayer", {styleMap : styleMap});
+        this.map.addLayers([highlightLayer]);
+        return highlightLayer;
+    }
+}
+
+function clearHighlightLayer(){
+    getHighlightLayer().destroyFeatures();
+}
