868cc7fb8f8a7fa3bea7bcf5b80538cba335b237
Jeremy Noonan <jeremynoonan@Jeremy-Noonans-MacBook.local>
2011-03-23 20:35:51 -0300

    recommited Aaron's code for pagination that got overwrote

--- iiv_annotation/iiv_annotation_flag_admin.tpl.php
+++ iiv_annotation/iiv_annotation_flag_admin.tpl.php
@@ -2,7 +2,72 @@
   //load the include files
   module_load_include('inc', 'iiv_annotation', 'inc/iiv_annotation_flagging');
   drupal_add_css(drupal_get_path('module','iiv_annotation') . '/css/flag.css');
-  $flagged = iiv_annotation_get_flagged(0, 100);
+
+  $rowsPerPage = 5; 
+  $pageNum = 1;
+  if (isset($_GET['page']))
+    {
+      $pageNum = $_GET['page'];
+    }
+
+  $offset = ($pageNum - 1) * $rowsPerPage; //offset for the number of pages.
+  $flagged = iiv_annotation_get_flagged($offset, $rowsPerPage);
+  $counterArray = iiv_annotation_get_count(); //get the number of elements in the array
+  $counterRow = db_fetch_array($counterArray);
+  $counter = $counterRow['numrows'];
+  $maxPage = ceil($counter/$rowsPerPage); //the maxi
+
+
+  echo('<center>');
+  $self = $_SERVER['REQUEST_URI'];
+  $queryCut= 0 - strlen(strstr($self,'?')); //Removes the page of a query from the variable $self
+  if ($queryCut != 0){
+    $self = substr($self, 0 ,$queryCut); //is ignored if queryCut is 0
+  }
+  $nav  = '';
+
+  //create the page table.
+  for($page = 1; $page <= $maxPage; $page++)
+  {
+    if ($page == $pageNum)
+    {
+       $nav .= " $page "; // no need to create a link to current page
+    }
+    else
+    {
+       $nav .= " <a href=\"$self?page=$page\">$page</a> ";
+    } 
+ }
+
+  if ($pageNum > 1)
+  {
+     $page  = $pageNum - 1;
+     $prev  = " <a href=\"$self?page=$page\">[Prev]</a> ";
+  
+    $first = " <a href=\"$self?page=1\">[First Page]</a> ";
+  } 
+  else
+  {
+     $prev  = '&nbsp;'; // we're on page one, don't print previous link
+     $first = '&nbsp;'; // nor the first page link
+  }
+
+  if ($pageNum < $maxPage)
+  {
+     $page = $pageNum + 1;
+     $next = " <a href=\"$self?page=$page\">[Next]</a> ";
+
+     $last = " <a href=\"$self?page=$maxPage\">[Last Page]</a> ";
+  } 
+  else
+  {
+     $next = '&nbsp;'; // we're on the last page, don't print next link
+     $last = '&nbsp;'; // nor the last page link
+  }
+  // print the navigation link
+  echo $first . $prev . $nav . $next . $last;
+  echo ('</center>');
+  //Create the table and elements.
 ?>
 <fieldset>
 	<legend><? echo t('Flagged annotations');?></legend>
@@ -31,6 +96,3 @@
 	  ?>
 	</table>
 </fieldset>
-
-    
-	
